a4f5311ebb0641d3fdc22a2a447cc1b1107fcae0
smackem <sm4ckem@outlook.com>
2020-11-29 19:56:43 +0100

    - doing yln lua integration

--- yln/luabind/luabind.c
+++ yln/luabind/luabind.c
@@ -2,22 +2,35 @@
 // Created by Philip Boger on 28.11.20.
 //
 
-#include <imaging.h>
-#include "luabind.h"
 #include <lualib.h>
 #include <lauxlib.h>
+#include <imaging.h>
+#include "luabind.h"
 
 void test_lua() {
     int error;
     lua_State *L = luaL_newstate();   // opens Lua
     luaL_openlibs(L);
-    const char *source = "print(\"hello to \", 1 + 1)";
+    const char *source = "print(\"hello from lua!\")\n"
+                         "result = 100\n";
 
-    error = luaL_loadbuffer(L, source, strlen(source), "line") ||
-            lua_pcall(L, 0, 0, 0);
-    if (error) {
-        TRACE("%s", lua_tostring(L, -1));
-        lua_pop(L, 1);  // pop error message from the stack
-    }
+    do {
+        error = luaL_loadbuffer(L, source, strlen(source), "line") ||
+                lua_pcall(L, 0, 0, 0);
+
+        if (error) {
+            TRACE("%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);  // pop error message from the stack
+            break;
+        }
+        int type = lua_getglobal(L, "result");
+        if (type != LUA_TNUMBER) {
+            TRACE("type: %d\n", type);
+            break;
+        }
+        lua_Integer result = luaL_checkinteger(L, -1);
+        wprintf(L"lua result: %d\n", result);
+    } ONCE;
+
     lua_close(L);
 }
